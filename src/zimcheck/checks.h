0d13bb175340db3e950ac9c40dd5b4e6282d49b5
Veloman Yunkan <veloman.yunkan@gmail.com>
2021-01-05 21:13:23 +0400

    Reporting of status in zimcheck JSON output mode

--- src/zimcheck/checks.h
+++ src/zimcheck/checks.h
@@ -75,6 +75,18 @@ class ErrorLogger {
 
     const std::string& indentation() const { return m_indentation; }
 
+    static std::string formatForJSON(const std::string& s) {
+      return "'" + s + "'";
+    }
+
+    static std::string formatForJSON(const char* s) {
+      return formatForJSON(std::string(s));
+    }
+
+    static const char* formatForJSON(bool b) {
+      return b ? "true" : "false";
+    }
+
   public:
     explicit ErrorLogger(bool _jsonOutputMode = false)
       : jsonOutputMode(_jsonOutputMode)
@@ -100,10 +112,11 @@ class ErrorLogger {
       }
     }
 
-    void addInfo(const std::string& key, const std::string& value) {
+    template<class T>
+    void addInfo(const std::string& key, const T& value) {
       if ( jsonOutputMode ) {
         std::cout << sep << indentation()
-                  << "'" << key << "' : '" << value << "'"
+                  << "'" << key << "' : " << formatForJSON(value)
                   << std::flush;
         sep = ",\n";
       }
--- src/zimcheck/zimcheck.cpp
+++ src/zimcheck/zimcheck.cpp
@@ -359,8 +359,10 @@ int zimcheck (const std::vector<const char*>& args)
                 }
         */
 
+        const bool overallStatus = error.overallStatus();
+        error.addInfo("status", overallStatus);
         error.report(error_details);
-        if( error.overallStatus())
+        if( overallStatus )
         {
             error.infoMsg("[INFO] Overall Test Status: Pass");
             status_code = PASS;
