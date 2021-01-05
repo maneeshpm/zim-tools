289d3ba5099464af917e90417cc450d79398c1f3
Veloman Yunkan <veloman.yunkan@gmail.com>
2021-01-05 22:55:40 +0400

    Reporting requested checks in zimcheck JSON output

--- src/zimcheck/checks.cpp
+++ src/zimcheck/checks.cpp
@@ -28,7 +28,6 @@ std::unordered_map<TestType, std::pair<LogTag, std::string>> errormapping = {
     { TestType::URL_INTERNAL,  {LogTag::ERROR, "Invalid internal links found"}},
     { TestType::URL_EXTERNAL,  {LogTag::ERROR, "Invalid external links found"}},
     { TestType::MIME,       {LogTag::ERROR, "Incoherent mimeType found"}},
-    { TestType::OTHER,      {LogTag::ERROR, "Other errors found"}}
 };
 
 } // unnamed namespace
